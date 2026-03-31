da8da97515dcdf903785a92e8925f6ed174a8988
realyoking <proeditdude@gmail.com>
2026-04-01 07:47:17 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,41 @@
+// 這個檔案需要放在與 HTML 代碼相同的目錄下。命名為 sw.js。
+const CACHE_NAME = 'family-trip-v1';
+const urlsToCache = [
+  '.', // 緩存當前頁面 (HTML 檔案)
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // 如果在緩存中找到，就返回；否則發起網絡請求
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
